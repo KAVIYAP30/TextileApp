62e7ad2109d669e2dc5b4fcbe1802341a3610a8d
Kaviya P <129056758+KAVIYAP30@users.noreply.github.com>
2024-04-28 17:24:07 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,6 @@ import foodRouter from './routers/food.router.js';
 import userRouter from './routers/user.router.js';
 import orderRouter from './routers/order.router.js';
 import uploadRouter from './routers/upload.router.js';
-
 import { dbconnect } from './config/database.config.js';
 import path, { dirname } from 'path';
 dbconnect();
@@ -28,6 +27,18 @@ app.use('/api/foods', foodRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 app.use('/api/upload', uploadRouter);
+
+//static files
+// Define the absolute directory path to the client build directory
+const clientBuildPath = new URL("./client/build", import.meta.url).pathname;
+
+// Serve static files from the client build directory
+app.use(express.static(clientBuildPath));
+
+// Route handler for all other requests, serving index.html
+app.get('*', function(req, res) {
+  res.sendFile(path.join(clientBuildPath, "index.html"));
+})
 
 const publicFolder = path.join(__dirname, 'public');
 app.use(express.static(publicFolder));
