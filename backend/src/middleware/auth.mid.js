e2ee3d051842086a3b0073a402cd44b33572767c
Kaviya P <129056758+KAVIYAP30@users.noreply.github.com>
2024-05-01 18:37:18 +0530

    Update auth.mid.js

--- backend/src/middleware/auth.mid.js
+++ backend/src/middleware/auth.mid.js
@@ -1,12 +1,11 @@
-import { verify } from 'jsonwebtoken';
-import { UNAUTHORIZED } from '../constants/httpStatus.js';
+import jwt from 'jsonwebtoken';
 
 export default (req, res, next) => {
   const token = req.headers.access_token;
   if (!token) return res.status(UNAUTHORIZED).send();
 
   try {
-    const decoded = verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
   } catch (error) {
     res.status(UNAUTHORIZED).send();
